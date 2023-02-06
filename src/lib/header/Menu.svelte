d808ecb89c3fb4a57c585aa603bf6a8ecfff273d
Robert Silve <robert@silve.net>
2023-02-06 20:39:28 +0100

    feat: better management on menu close

--- src/lib/header/Menu.svelte
+++ src/lib/header/Menu.svelte
@@ -3,20 +3,27 @@
     import Upload from "./Upload.svelte";
     import MenuIcon from "../components/icons/MenuIcon.svelte";
 
+    let labelElement;
+
+    function close() {
+        labelElement?.click()
+    }
+
+
 </script>
 
 <div class="relative">
-    <label for="top-left-menu" class="btn btn-outline text-2xl">
+    <label bind:this={labelElement} for="top-left-menu" class="btn btn-outline text-2xl">
         <MenuIcon alt="Application menu"/>
     </label>
     <input type="checkbox" id="top-left-menu" class="menu-toggle hidden"/>
     <label for="top-left-menu" class="modal-menu-background"></label>
     <ul class="modal-menu menu bg-base-100 w-56 p-2 rounded border absolute top-full ">
         <li>
-            <Download/>
+            <Download onComplete={close}/>
         </li>
         <li>
-            <Upload/>
+            <Upload onComplete={close}/>
         </li>
     </ul>
 
