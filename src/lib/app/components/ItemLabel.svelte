fb5b64c8114b17e7d9ffe4a60c55d513ba9ef049
Robert Silve <robert@silve.net>
2023-02-26 17:23:05 +0100

    feat: refactor to avoid stores in components

--- src/lib/app/components/ItemLabel.svelte
+++ src/lib/app/components/ItemLabel.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
     import { createEventDispatcher } from 'svelte'
-    import type { Item, TagColors } from '../../model'
+    import type { ItemEnhanced, TagColors } from '../../model'
     import { useDblClick } from '../../utils'
     import TagBadge from './TagBadge.svelte'
 
     const dispatch = createEventDispatcher()
     const [singleClick, dblClick] = useDblClick()
 
-    export let item: Item
-    export let duplicate: boolean = false
+    export let item: ItemEnhanced
     export let selected: boolean = false
     export let colorMap: TagColors = {}
 
@@ -28,7 +27,7 @@
 <div style="text-decoration: {selected ? 'underline' : 'none'}">
     <a on:click={select} on:dblclick={edit} href={'#'} tabIndex="-1">
         {item.name}
-        {#if duplicate}
+        {#if item.duplicate}
             <span class="badge-warning badge badge-xs" />
         {/if}
         <span class="inline-flex items-baseline gap-1">
--- src/lib/app/components/blip/BlipStack.svelte
+++ src/lib/app/components/blip/BlipStack.svelte
@@ -1,18 +1,16 @@
 <script lang="ts">
     import type { Item } from '../../../model'
     import { toArc } from '../../../utils'
-    import { selected } from '../../../stores'
 
     export let radius: number
     export let item: Item
-    let isSelected = false
-    $: isSelected = $selected?.index === item.index
+    export let selected = false
 </script>
 
 <div
     class="absolute -translate-x-1/2 -translate-y-1/2 transition hover:z-50"
-    class:scale-[1.2]={isSelected}
-    class:brightness-125={isSelected}
+    class:scale-[1.2]={selected}
+    class:brightness-125={selected}
     style="top:{radius + toArc(item, radius / 5).x}%; left:{radius +
         toArc(item, radius / 5).y}%;"
     data-tip={item.name}
