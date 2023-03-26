03f43764abe578993b24ad1328b7402580c54ed9
Robert Silve <robert@silve.net>
2023-03-26 12:16:00 +0200

    feat: add share feature

--- src/lib/app/GlobalContext.ts
+++ src/lib/app/GlobalContext.ts
@@ -8,6 +8,11 @@ export function createContext(dataString?: string) {
 
 	context.items.subscribe((items) => loader.updateItems(items));
 	context.adoptionLevels.subscribe((levels) => loader.updateLevels(levels));
+	context.share.subscribe((b64) => {
+		const url = new URL(document.location.href);
+		url.searchParams.set('data', encodeURIComponent(b64));
+		console.log(url.toString());
+	});
 
 	setContext(GLOBAL_CONTEXT, context);
 }
