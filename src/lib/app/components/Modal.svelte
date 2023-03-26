921ea02a6b32a640e3021c216bb2ef58152db112
Robert Silve <robert@silve.net>
2023-03-26 17:41:22 +0200

    feat: esc for close modal

--- src/lib/app/components/Modal.svelte
+++ src/lib/app/components/Modal.svelte
@@ -1,3 +1,15 @@
+<script lang="ts">
+	import { navigate } from 'svelte-navigator';
+
+	function handleKeydown(event: KeyboardEvent) {
+		if (event.key === 'Escape') {
+			navigate('/');
+		}
+	}
+</script>
+
+<svelte:body on:keydown={handleKeydown} />
+
 <div class="modal modal-open">
 	<div class="modal-box m-auto w-11/12 max-w-5xl">
 		<slot />
