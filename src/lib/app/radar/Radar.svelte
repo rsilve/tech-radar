8920f10b88045051071ca35dcf67413e95bc320a
Robert Silve <robert@silve.net>
2023-03-30 22:16:12 +0200

    feat: tag badge refactoring

--- src/lib/app/radar/Radar.svelte
+++ src/lib/app/radar/Radar.svelte
@@ -32,10 +32,10 @@
 </script>
 
 <div class="radar grid h-full justify-start">
-	<Quarter quarter={1} on:edit={edit} on:select={select} />
-	<Quarter quarter={2} on:edit={edit} on:select={select} />
-	<Quarter quarter={3} on:edit={edit} on:select={select} />
-	<Quarter quarter={4} on:edit={edit} on:select={select} />
+	<Quarter on:edit={edit} on:select={select} quarter={1} />
+	<Quarter on:edit={edit} on:select={select} quarter={2} />
+	<Quarter on:edit={edit} on:select={select} quarter={3} />
+	<Quarter on:edit={edit} on:select={select} quarter={4} />
 	<div
 		class="relative aspect-square justify-self-center portrait:w-full portrait:max-w-full landscape:h-full landscape:max-h-full"
 		style="grid-area: target"
