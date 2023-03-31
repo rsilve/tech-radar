084db8654c70d2117b602b6ac4f687aac3c99ea5
Robert Silve <robert@silve.net>
2023-03-31 06:37:56 +0200

    feat: tag badge refactoring

--- src/lib/app/components/TagBadge.svelte
+++ src/lib/app/components/TagBadge.svelte
@@ -7,5 +7,5 @@
 </script>
 
 <span class="group badge badge-xs" style="text-shadow: 0 0 1px black; background-color: {colorMap[tag] || '#cccccc'}">
-	<span class="w-0 overflow-hidden  whitespace-nowrap group-hover:w-fit">{tag} {count ?? ''}</span>
+	<span class="w-0 overflow-hidden  whitespace-nowrap transition-all group-hover:w-fit">{tag} {count ?? ''}</span>
 </span>
