6f8bec3742f9730b947e18cbab39435cd3a7d2c7
Robert Silve <robert@silve.net>
2023-04-03 22:16:09 +0200

    feat: fix arrow orientation

--- src/lib/app/components/radar/blip/BlipGoOut.svelte
+++ src/lib/app/components/radar/blip/BlipGoOut.svelte
@@ -4,7 +4,10 @@
 	import { unitToRadial } from '../../../../utils';
 
 	export let item: ItemEnhanced;
-	let angle = unitToRadial(item).x - Math.PI / 2;
+	let angle = 0;
+	$: {
+		angle = unitToRadial(item).x - Math.PI / 2;
+	}
 </script>
 
 <div class="translate-x-5 text-neutral" style="grid-area: 1 / 1 / 2 / 2" style:rotate="-{angle}rad">
