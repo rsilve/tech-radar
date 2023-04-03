7fa8e32484decc7fbd91484ffa0e8a428e40f8a9
Robert Silve <robert@silve.net>
2023-04-03 20:45:36 +0200

    feat: use arrow for in direction

--- src/lib/app/components/radar/blip/BlipGoIn.svelte
+++ src/lib/app/components/radar/blip/BlipGoIn.svelte
@@ -1,15 +1,24 @@
 <script lang="ts">
 	import type { Quarter } from '../../../../model';
+	import ArrowRightIcon from '../../icons/ArrowRightIcon.svelte';
 
 	export let quarter: Quarter;
 </script>
 
 {#if quarter === 1}
-	<div class="absolute bottom-0 left-0 h-0 w-0 border-2 border-t-0 border-r-0 border-slate-800/60 p-1" />
+	<div class="absolute bottom-0 left-0 -translate-x-2 translate-y-2 rotate-[135deg] text-neutral">
+		<ArrowRightIcon />
+	</div>
 {:else if quarter === 2}
-	<div class="absolute bottom-0 right-0 h-0 w-0 border-2 border-t-0 border-l-0 border-slate-800/60 p-1" />
+	<div class="absolute bottom-0 right-0 translate-x-2 translate-y-2 rotate-45 text-neutral">
+		<ArrowRightIcon />
+	</div>
 {:else if quarter === 3}
-	<div class="absolute top-0 right-0 h-0 w-0 border-2 border-b-0 border-l-0 border-slate-800/60 p-1" />
+	<div class="absolute top-0 right-0 translate-x-2 -translate-y-2 -rotate-45 text-neutral">
+		<ArrowRightIcon />
+	</div>
 {:else}
-	<div class="absolute top-0 left-0 h-0 w-0 border-2 border-b-0 border-r-0 border-slate-800/60 p-1" />
+	<div class="absolute top-0 left-0 -translate-x-2 -translate-y-2 -rotate-[135deg] text-neutral">
+		<ArrowRightIcon />
+	</div>
 {/if}
