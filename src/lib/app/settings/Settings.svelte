35d5ac03666504838ee27b9e17d85031fcbb18f2
Robert Silve <robert@silve.net>
2023-04-09 10:10:36 +0200

    feat: refactor settings

--- src/lib/app/settings/Settings.svelte
+++ src/lib/app/settings/Settings.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
 	import ModalFooter from '../components/ModalFooter.svelte';
 	import { navigate } from 'svelte-navigator';
-	import { adoptionLevels as origin } from '../../model';
+	import { adoptionLevels as originLevels } from '../../model';
 	import { getContext } from 'svelte';
 	import { GLOBAL_CONTEXT } from '../GlobalContext';
 	import type { AppContext } from '../../stores';
+	import SettingsAdoptionLevels from './SettingsAdoptionLevels.svelte';
 
 	const { adoptionLevels } = getContext<AppContext>(GLOBAL_CONTEXT);
 
-	let levels = { ...origin, ...$adoptionLevels };
+	let levels = { ...originLevels, ...$adoptionLevels };
 
 	function cancel() {
 		navigate('/');
@@ -23,56 +24,7 @@
 <div>
 	<h1 class="mb-0.5 mt-2 text-xl font-bold">Settings</h1>
 	<form on:submit|preventDefault={submit}>
-		<div>Configure adoption level labels :</div>
-		<div class="grid grid-cols-4 gap-x-4 pl-8">
-			<div class="form-control w-full max-w-xs">
-				<label class="label" for="adopt">
-					<span class="label-text">{origin[1]}</span>
-					<span
-						class="label-text-alt cursor-pointer"
-						on:click={() => (levels[1] = origin[1])}
-						on:keypress={() => (levels[1] = origin[1])}>reset</span
-					>
-				</label>
-				<input bind:value={levels[1]} class="input-bordered input" id="adopt" placeholder="Type here" type="text" />
-			</div>
-
-			<div class="form-control w-full max-w-xs">
-				<label class="label" for="trial">
-					<span class="label-text">{origin[2]}</span>
-					<span
-						class="label-text-alt cursor-pointer"
-						on:click={() => (levels[2] = origin[2])}
-						on:keypress={() => (levels[2] = origin[2])}>reset</span
-					>
-				</label>
-				<input bind:value={levels[2]} class="input-bordered input" id="trial" placeholder="Type here" type="text" />
-			</div>
-
-			<div class="form-control w-full max-w-xs">
-				<label class="label" for="assess">
-					<span class="label-text">{origin[3]}</span>
-					<span
-						class="label-text-alt cursor-pointer"
-						on:click={() => (levels[3] = origin[3])}
-						on:keypress={() => (levels[3] = origin[3])}>reset</span
-					>
-				</label>
-				<input bind:value={levels[3]} class="input-bordered input" id="assess" placeholder="Type here" type="text" />
-			</div>
-
-			<div class="form-control w-full max-w-xs">
-				<label class="label" for="hold">
-					<span class="label-text">{origin[4]}</span>
-					<span
-						class="label-text-alt cursor-pointer"
-						on:click={() => (levels[4] = origin[4])}
-						on:keypress={() => (levels[4] = origin[4])}>reset</span
-					>
-				</label>
-				<input bind:value={levels[4]} class="input-bordered input" id="hold" placeholder="Type here" type="text" />
-			</div>
-		</div>
+		<SettingsAdoptionLevels {levels} origin={originLevels} />
 	</form>
 
 	<ModalFooter>
---
+++ src/lib/app/settings/SettingsAdoptionLevels.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	import type { AdoptionLevels } from '../../model';
+
+	export let levels: AdoptionLevels;
+	export let origin: AdoptionLevels;
+</script>
+
+<div>Configure adoption level labels :</div>
+<div class="grid grid-cols-4 gap-x-4 pl-8">
+	<div class="form-control w-full max-w-xs">
+		<label class="label" for="adopt">
+			<span class="label-text">{origin[1]}</span>
+			<span
+				class="label-text-alt cursor-pointer"
+				on:click={() => (levels[1] = origin[1])}
+				on:keypress={() => (levels[1] = origin[1])}>reset</span
+			>
+		</label>
+		<input bind:value={levels[1]} class="input-bordered input" id="adopt" placeholder="Type here" type="text" />
+	</div>
+
+	<div class="form-control w-full max-w-xs">
+		<label class="label" for="trial">
+			<span class="label-text">{origin[2]}</span>
+			<span
+				class="label-text-alt cursor-pointer"
+				on:click={() => (levels[2] = origin[2])}
+				on:keypress={() => (levels[2] = origin[2])}>reset</span
+			>
+		</label>
+		<input bind:value={levels[2]} class="input-bordered input" id="trial" placeholder="Type here" type="text" />
+	</div>
+
+	<div class="form-control w-full max-w-xs">
+		<label class="label" for="assess">
+			<span class="label-text">{origin[3]}</span>
+			<span
+				class="label-text-alt cursor-pointer"
+				on:click={() => (levels[3] = origin[3])}
+				on:keypress={() => (levels[3] = origin[3])}>reset</span
+			>
+		</label>
+		<input bind:value={levels[3]} class="input-bordered input" id="assess" placeholder="Type here" type="text" />
+	</div>
+
+	<div class="form-control w-full max-w-xs">
+		<label class="label" for="hold">
+			<span class="label-text">{origin[4]}</span>
+			<span
+				class="label-text-alt cursor-pointer"
+				on:click={() => (levels[4] = origin[4])}
+				on:keypress={() => (levels[4] = origin[4])}>reset</span
+			>
+		</label>
+		<input bind:value={levels[4]} class="input-bordered input" id="hold" placeholder="Type here" type="text" />
+	</div>
+</div>
