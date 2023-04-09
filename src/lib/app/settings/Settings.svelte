133560075180ad023f58495bf09b8760e2aa4dc4
Robert Silve <robert@silve.net>
2023-04-09 10:25:53 +0200

    feat: add categories settings

--- src/lib/app/settings/Settings.svelte
+++ src/lib/app/settings/Settings.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
 	import ModalFooter from '../components/ModalFooter.svelte';
 	import { navigate } from 'svelte-navigator';
-	import { adoptionLevels as originLevels } from '../../model';
+	import { adoptionLevels as originLevels, categories as originCategories } from '../../model';
 	import { getContext } from 'svelte';
 	import { GLOBAL_CONTEXT } from '../GlobalContext';
 	import type { AppContext } from '../../stores';
 	import SettingsAdoptionLevels from './SettingsAdoptionLevels.svelte';
+	import SettingsCategories from './SettingsCategories.svelte';
 
-	const { adoptionLevels } = getContext<AppContext>(GLOBAL_CONTEXT);
+	const { adoptionLevels, categories } = getContext<AppContext>(GLOBAL_CONTEXT);
 
 	let levels = { ...originLevels, ...$adoptionLevels };
+	let categoryLabels = { ...originCategories, ...$categories };
 
 	function cancel() {
 		navigate('/');
@@ -17,16 +19,23 @@
 
 	function submit() {
 		adoptionLevels.update(() => levels);
+		categories.update(() => categoryLabels);
 		navigate('/');
 	}
 </script>
 
 <div>
 	<h1 class="mb-0.5 mt-2 text-xl font-bold">Settings</h1>
+	<p class="text-accent">
+		Configure your radar. Configuration will be store with the current radar. It does not affect other radar.
+	</p>
+	<span class="divider" />
 	<form on:submit|preventDefault={submit}>
 		<SettingsAdoptionLevels {levels} origin={originLevels} />
+		<span class="divider" />
+		<SettingsCategories categories={categoryLabels} origin={originCategories} />
 	</form>
-
+	<span class="divider" />
 	<ModalFooter>
 		<button class="btn" on:click={submit} type="button"> Apply</button>
 		<button class="btn-outline btn" on:click={cancel} type="button"> Close</button>
---
+++ src/lib/app/settings/SettingsCategories.svelte
@@ -0,0 +1,81 @@
+<script lang="ts">
+	import type { Categories } from '../../model';
+
+	export let categories: Categories;
+	export let origin: Categories;
+</script>
+
+<div>Configure categories labels :</div>
+<div class="grid grid-cols-4 gap-x-4 pl-8">
+	<div class="form-control w-full max-w-xs">
+		<label class="label" for="adopt">
+			<span class="label-text">{origin[1]}</span>
+			<span
+				class="label-text-alt cursor-pointer"
+				on:click={() => (categories[1] = origin[1])}
+				on:keypress={() => (categories[1] = origin[1])}>reset</span
+			>
+		</label>
+		<textarea
+			bind:value={categories[1]}
+			class="textarea-bordered textarea"
+			id="adopt"
+			placeholder="Type here"
+			type="text"
+		/>
+	</div>
+
+	<div class="form-control w-full max-w-xs">
+		<label class="label" for="trial">
+			<span class="label-text">{origin[2]}</span>
+			<span
+				class="label-text-alt cursor-pointer"
+				on:click={() => (categories[2] = origin[2])}
+				on:keypress={() => (categories[2] = origin[2])}>reset</span
+			>
+		</label>
+		<textarea
+			bind:value={categories[2]}
+			class="textarea-bordered textarea"
+			id="trial"
+			placeholder="Type here"
+			type="text"
+		/>
+	</div>
+
+	<div class="form-control w-full max-w-xs">
+		<label class="label" for="assess">
+			<span class="label-text">{origin[3]}</span>
+			<span
+				class="label-text-alt cursor-pointer"
+				on:click={() => (categories[3] = origin[3])}
+				on:keypress={() => (categories[3] = origin[3])}>reset</span
+			>
+		</label>
+		<textarea
+			bind:value={categories[3]}
+			class="textarea-bordered textarea"
+			id="assess"
+			placeholder="Type here"
+			type="text"
+		/>
+	</div>
+
+	<div class="form-control w-full max-w-xs">
+		<label class="label" for="hold">
+			<span class="label-text">{origin[4]}</span>
+			<span
+				class="label-text-alt cursor-pointer"
+				on:click={() => (categories[4] = origin[4])}
+				on:keypress={() => (categories[4] = origin[4])}>reset</span
+			>
+		</label>
+		<textarea
+			bind:value={categories[4]}
+			class="textarea-bordered textarea"
+			id="hold"
+			placeholder="Type here"
+			type="text"
+		/>
+	</div>
+</div>
